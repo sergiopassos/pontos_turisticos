eaff38b53a2ce5c2097632cf8da6e39b14071b1f
sergiopassos <sergio.passos02@gmail.com>
2019-08-15 20:25:17 -0300

    Create apps comentarios e avaliacoes

--- core/models.py
+++ core/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from atracoes.models import Atracao
+from avaliacoes.models import Avaliacao
+from comentarios.models import Comentario
+
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
     atracoes = models.ManyToManyField(Atracao)
+    comentarios = models.ManyToManyField(Comentario)
+    avaliacoes = models.ManyToManyField(Avaliacao)
 
     def __str__(self):
         return self.nome
--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'core',
     'atracoes',
+    'comentarios',
+    'avaliacoes',
 
 ]
 
