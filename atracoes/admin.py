5cbc199637e9655203e30b8cba5933d3d3fb9c34
sergiopassos <sergio.passos02@gmail.com>
2019-08-15 19:57:23 -0300

    Create app Atracoes

---
+++ atracoes/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from atracoes.models import Atracao
+
+admin.site.register(Atracao)
--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core',
     'rest_framework',
+    'core',
+    'atracoes',
+
 ]
 
 MIDDLEWARE = [
