be9e2f22ea51cb0fd22b21892b6ace156fe47b9c
sergiopassos <sergio.passos02@gmail.com>
2019-08-20 19:56:21 -0300

    Adicionado campo foto e aplicado filtro no queryset

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 from comentarios.api.viewsets import ComentarioViewSet
 from core.api.viewsets import PontoTuristicoViewSet
@@ -33,4 +35,4 @@ router.register(r'avaliacoes', AvaliacaoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
