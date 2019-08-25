abc2783b7ac0ffd9ff61770241389c171f2380e3
sergiopassos <sergio.passos02@gmail.com>
2019-08-24 22:12:13 -0300

    Create ManyToMany relationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
+
+from atracoes.models import Atracao
 from core.models import PontoTuristico
 
 from atracoes.api.serializers import AtracaoSerializer
@@ -8,14 +10,28 @@ from enderecos.api.serializers import EnderecoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
-    endereco = EnderecoSerializer()
+    endereco = EnderecoSerializer(read_only=True)
     descricao_completa = SerializerMethodField()
+
     class Meta:
         model = PontoTuristico
         fields = ('id', 'nome', 'descricao', 'aprovado', 'foto',
                   'atracoes', 'comentarios', 'avaliacoes', 'endereco',
                   'descricao_completa', 'descricao_completa2',
                   )
+        read_only_fields = ('comentarios', 'avaliacoes')
+
+    def cria_atracoes(self, atracoes, ponto):
+        for atracao in atracoes:
+            at = Atracao.objects.create(**atracao)
+            ponto.atracoes.add(at)
+
+    def create(self, validated_data):
+        atracoes = validated_data['atracoes']
+        del validated_data['atracoes']
+        ponto = PontoTuristico.objects.create(**validated_data)
+        self.cria_atracoes(atracoes, ponto)
+        return ponto
 
 
     def get_descricao_completa(self, obj):
--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['pontos-turisticos-sergiopassos.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pontos-turisticos-sergiopassos.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
