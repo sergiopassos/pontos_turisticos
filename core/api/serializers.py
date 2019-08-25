4a55d995274b3b6adc01ec6c6a7b808e35318f18
sergiopassos <sergio.passos02@gmail.com>
2019-08-24 22:24:33 -0300

    Create ForeignKeys relationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
-from atracoes.models import Atracao
-from core.models import PontoTuristico
+from core.models import PontoTuristico, Atracao
 
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
+from enderecos.models import Endereco
 
 
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
-    endereco = EnderecoSerializer(read_only=True)
+    endereco = EnderecoSerializer()
     descricao_completa = SerializerMethodField()
 
     class Meta:
@@ -29,8 +29,17 @@ class PontoTuristicoSerializer(ModelSerializer):
     def create(self, validated_data):
         atracoes = validated_data['atracoes']
         del validated_data['atracoes']
+
+        endereco = validated_data['endereco']
+        del validated_data['endereco']
+
         ponto = PontoTuristico.objects.create(**validated_data)
         self.cria_atracoes(atracoes, ponto)
+
+        end = Endereco.objects.create(**endereco)
+        ponto.endereco = end
+        ponto.save()
+
         return ponto
 
 
